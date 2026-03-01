b55dd64adedd30a9748232984026613fd2a78b39
Jarden Pan <30641248+panjd123@users.noreply.github.com>
2026-03-01 23:58:20 +0800

    Update bashrc_common.sh

--- bashrc_common.sh
+++ bashrc_common.sh
@@ -216,8 +216,12 @@ alias va=a
 
 alias da=deactivate
 
-hf_download() {
+hf_mirror_download() {
   HF_ENDPOINT=https://hf-mirror.com python3 -c "from huggingface_hub import snapshot_download; snapshot_download('$1')"
+}
+alias hf-mirror-download='hf_mirror_download'
+hf_download() {
+ python3 -c "from huggingface_hub import snapshot_download; snapshot_download('$1')"
 }
 alias hf-download='hf_download'
 
